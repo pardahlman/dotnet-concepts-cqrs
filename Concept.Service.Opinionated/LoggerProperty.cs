91f6a05192f993e3f5723146c6b76b48e4be86d6
par.dahlman <par.dahlman@fellowdeveloper.se>
2017-10-17 12:57:45 +0200

    Reference nuget for concept.service.x

--- Concept.Service.Opinionated/LoggerProperty.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Concept.Service.Opinionated
-{
-  public class LoggerProperty
-  {
-    public const string Application = "Application";
-    public const string Version = "Version";
-  }
-}
